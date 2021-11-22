a50a8a49e0d707a5733971891107903142781681
WJava <1140480798@qq.com>
2021-11-22 22:05:06 +0800

    1122

---
+++ N 叉树的最大深度.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+// @solution-sync:begin
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+class Solution {
+    public int maxDepth(Node root) {
+        
+    }
+}
